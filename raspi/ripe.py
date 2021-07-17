69d301a4667b528204d5407a1fa758e0c177e0bd
knusperkrone <aron.metzig@fau.de>
2021-07-17 18:19:19 +0200

    container is life

--- raspi/ripe.py
+++ raspi/ripe.py
@@ -1,10 +1,11 @@
+import os
 import sys
 
 from app import loop
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        base_url = str(sys.argv[1])
+    base_url = os.environ.get('BASE_URL')
+    if base_url is not None:
         loop.kickoff(base_url)
     else:
         loop.kickoff()
--- raspi/start_ripe.sh
+++ raspi/start_ripe.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-while true; do
-    kill $(pgrep -f ripe | grep -v ^$$\$) > /dev/null 2>&1
-    python3 -u ripe.py http://192.168.178.37:8080/api
-done
+docker build . -t raspi_sensor
+docker run -d --restart unless-stopped --net=host --privileged -e "BASE_URL=http://192.168.178.37:8080/api" raspi_sensor
