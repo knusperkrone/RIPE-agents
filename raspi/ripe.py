d0fa46c4d54ae93b79b49bddd80771501015c04d
Aron Metzig <aron.metzig@flexperto.com>
2023-01-29 23:09:53 +0100

    Revert "Test commit for hot-update"
    This reverts commit 0351aed348ac40b4b9d64b1d5bd9a7639f957d5f.

--- raspi/ripe.py
+++ raspi/ripe.py
@@ -4,8 +4,6 @@ from app import loop
 
 DEFAULT_URL = 'https://ripe.knukro.com/api'
 
-print('Test')
-
 if __name__ == '__main__':
     base_url = os.environ.get('BASE_URL')
     if base_url is not None:
