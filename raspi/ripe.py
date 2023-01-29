0351aed348ac40b4b9d64b1d5bd9a7639f957d5f
Aron Metzig <aron.metzig@flexperto.com>
2023-01-29 23:06:40 +0100

    Test commit for hot-update

--- raspi/ripe.py
+++ raspi/ripe.py
@@ -4,6 +4,8 @@ from app import loop
 
 DEFAULT_URL = 'https://ripe.knukro.com/api'
 
+print('Test')
+
 if __name__ == '__main__':
     base_url = os.environ.get('BASE_URL')
     if base_url is not None:
