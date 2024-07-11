8029b36c799e4fff58638c82f2a107812907fc49
Aron Metzig <aron.metzig@flexperto.com>
2024-07-11 22:02:14 +0200

    Produce according sensor

--- raspi/app/device/sensor/__init__.py
+++ raspi/app/device/sensor/__init__.py
@@ -8,6 +8,6 @@ def create_sensor_from_json(json) -> Sensor:
     if type == "miflora":
         return MiFloraSensor(json["mac"])
     elif type == "lywsd03mmc":
-        return Sensor()
+        return Lywsd03mmcSensor()
     else:
         raise NotImplementedError(f"Invalid agent-type: {type}")
