a37a5efbd94536f1c5767f185025f54941640b81
Aron Metzig <aron.metzig@flexperto.com>
2024-07-11 22:24:11 +0200

    Fixing accidental upcast

--- raspi/app/util/math.py
+++ raspi/app/util/math.py
@@ -1,13 +1,19 @@
 import functools
+from typing import Optional, Union
 
-def argmin(values: list) -> int or None:
-    if len (values) == 0:
+
+def argmin(values: list[Union[int, float]]) -> Optional[Union[int, float]]:
+    if len(values) == 0:
         return None
-    if len (values) == 1:
+    if len(values) == 1:
         return values[0]
     return functools.reduce(min, values)
 
-def average(values: list) -> int or None:
+
+def average(values: list[Union[int, float]]) -> Optional[Union[int, float]]:
     if len(values) == 0:
         return None
+
+    if isinstance(values[0], int):
+        return int(sum(values) / len(values))
     return sum(values) / len(values)
