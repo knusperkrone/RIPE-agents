e9457fb2f79bf75b93c32ffdf11220243c984c61
Aron Metzig <aron.metzig@flexperto.com>
2024-07-13 20:48:10 +0200

    Fallback for no  data

--- raspi/app/device/sensor/lywsd03mmc.py
+++ raspi/app/device/sensor/lywsd03mmc.py
@@ -1,10 +1,10 @@
 import lywsd03mmc
 
 from typing import Optional
-from app.backend import SensorData
-from .base import Sensor
 
-import lywsd03mmc
+from app.backend import SensorData
+from app.util.log import logger
+from .base import Sensor
 
 
 class Lywsd03mmcSensor(Sensor):
@@ -13,22 +13,29 @@ class Lywsd03mmcSensor(Sensor):
         super().__init__()
         self._mac = mac
         self._client = lywsd03mmc.Lywsd03mmcClient(self._mac)
+        self._last_data: Optional[lywsd03mmc.SensorDataBattery] = None
 
     async def get_sensor_data(self) -> Optional[SensorData]:
         try:
             data: lywsd03mmc.SensorDataBattery = self._client.data  # implicit connect
+            self._last_data = data
 
-            return SensorData(
-                battery=data.battery,
-                moisture=None,
-                light=None,
-                temperature=data.temperature,
-                conductivity=None,
-                humidity=data.humidity,
-            )
+            return self._transform_data(data)
         except Exception as e:
-            print(f"Failed to get sensor data: {e}")
+            logger.error(f"Failed to get sensor data: {e}")
+            if self._last_data:
+                return self._transform_data(self._last_data)
             return None
+
+    def _transform_data(self, data: lywsd03mmc.SensorDataBattery) -> SensorData:
+        return SensorData(
+            battery=data.battery,
+            moisture=None,
+            light=None,
+            temperature=data.temperature,
+            conductivity=None,
+            humidity=data.humidity,
+        )
 
     def __str__(self) -> str:
         return f"Lywsd03mmcSensor[{self._mac}]"
